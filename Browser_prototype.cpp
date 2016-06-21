2d3bf159e250ecce8a572f939e85734894dd0aef
wrighth <wrighth@ohsum01.ohsu.edu>
2016-06-21 14:55:26 -0700

    Revert "First Commit"
    This reverts commit feecaa752ad554226fe798cfc8d05d030f273ffc.

--- Browser_prototype.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Browser_prototype.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Browser_prototype, "Browser_prototype" );
